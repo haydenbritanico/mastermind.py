54710916aa6e13c67b578d06a98fd88e4e5b5b5a
Hayden Britanico <hayden.britanico@gmail.com>
2024-11-04 11:45:07 -0700

    Add files via upload

---
+++ mastermind.py
@@ -0,0 +1,40 @@
+#Hayden Britanico
+#mastermind.py
+#11.1.2024
+#Computer generates a random 4 digit number without repeats
+
+#10 tries
+#if correct and in right spot: "green"
+#if correct but wrong spot; "yellow"
+#outputs greens then yellows
+
+digits = ["1, 2, 3, 4, 5, 6, 7, 8, 9, 0"]
+random.shuffle(digits)
+secret = digits[:4]
+attempts < 10 
+attempt += 1
+
+print(secret);
+secret = input(int("enter a 4 digit number: "))
+
+
+while attempt < 10 and secret != [green, green, green, green]:
+    print 
+
+guess = input(int("enter a 4 digit number: "))
+
+    
+if guess[n] == secret [n]:
+    result.append("green")
+elif guess[n] in secret:
+
+if guess[n] >= secret [n] and if guess[n] <= secret [n]:
+    result.append("yellow")
+elif guess[n] in secret:
+
+
+
+
+results.sort()
+print(results)
+print(sorted(results))
